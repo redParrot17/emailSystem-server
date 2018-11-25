ab3b617ce85ed22e88907251c94fa1dfd8dbf4c3
redparrot17 <burnscr17@gcc.edu>
2018-11-25 17:11:24 -0500

    Added more in-depth method stubs

--- src/main/java/email_system/EmailServer.java
+++ src/main/java/email_system/EmailServer.java
@@ -1,6 +1,7 @@
 package email_system;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import server.TcpServer;
 import server.listener_references.Connection;
@@ -13,10 +14,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Type;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class EmailServer {
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().serializeNulls().setLenient().disableHtmlEscaping().create();
 
     private ConcurrentHashMap<Connection, Account> loggedInAccounts;
     private ConnectionListener connectionListener;
@@ -25,7 +30,7 @@ public class EmailServer {
     private EmailListener emailListener;
     private final TcpServer tcpServer;
 
-    private final static String ACCOUNT_FILENAME = "";
+    private final static String ACCOUNT_FILENAME = "accounts.json";
     private boolean running;
 
     /**
@@ -86,17 +91,13 @@ public class EmailServer {
      */
     private HashSet<Account> retrieveAllAccounts(String fileName) throws IOException {
         StringBuilder fileContents = new StringBuilder(); // write all data from the file into this variable
-        File openFile = new File(fileName); 
-        Scanner readFile = new Scanner(openFile);
-
-        while (readFile.hasNext()) {
-        	fileContents.append(readFile.next());
+        Scanner readFile = new Scanner(new File(fileName));
+        while (readFile.hasNextLine()) {
+            fileContents.append(readFile.nextLine()).append("\n");
         }
-
         Type type = new TypeToken<HashSet<Account>>() {}.getType();
-        HashSet<Account> accounts = allAccounts = new Gson().fromJson(fileContents.toString(), type);
+        HashSet<Account> accounts = allAccounts = GSON.fromJson(fileContents.toString(), type);
         readFile.close();
-
         return accounts != null ? accounts : new HashSet<>();
     }
 
@@ -107,9 +108,10 @@ public class EmailServer {
      * @throws IOException If there was a problem saving the accounts to the specified file
      */
     private void saveAllAccounts(String fileName) throws IOException {
-    	PrintWriter outputFile = new PrintWriter("output.txt");
-        String json = new Gson().toJson(allAccounts); // the data to be written to the file
+    	PrintWriter outputFile = new PrintWriter(new File(fileName));
+        String json = GSON.toJson(allAccounts); // the data to be written to the file
         Scanner inputFile = new Scanner(json);
+        inputFile.useDelimiter("");
         while (inputFile.hasNext()) {
         	outputFile.printf(inputFile.next());
         }
@@ -150,19 +152,78 @@ public class EmailServer {
             String action = command.getCommand();
             String data = command.getArguments();
 
-            //TODO: handle login scenario including adding the connection/account to the HashMap and replying if it was successful
-            // reply if the login was successful or invalid
-            //TODO: handle creating a new account
-            // make sure the account username is unique
-            // ignore letter case; lowercase and uppercase shouldn't matter
-            //TODO: handle user sending a new email
-            //TODO: handle user deleting an existing email
-            //TODO: handle user marking an email as read
-            //TODO: handle user requesting their emails
+            switch (action) {
 
-            // Don't assume the data is valid. Make sure to have error prevention
-            // so that this method never throws an uncaught exception
+                case "login":
+                    try {
+                        String[] credentials = data.split(",",2);
+                        credentials[0] = credentials[0].toLowerCase();
+                        char[] pass = new char[credentials[1].length()];
+                        for (int i=0; i<pass.length; i++) {
+                            pass[i] = credentials[1].charAt(i);
+                        }
+                        Account account = allAccounts.stream().filter(a -> a.getEmail_address().equals(credentials[0])).findFirst().get();
+                        if (account.checkPassword(pass)) {
 
+                            //TODO: Call the connection#replyCommand method with "login" as the command and "valid" as the argument
+                            //TODO: .put a new entry into loggedInAccounts with connection and account as the parameters
+
+                        } else {
+                            connection.replyCommand("login", "invalid");
+                        }
+                    } catch (Exception e) {
+
+                        //TODO: Call the connection#replyCommand method with "login" as the command and "invalid" as the argument
+                    }
+                    break;
+
+                case "new-account":
+                    String[] credentials = data.split(",",2);
+                    credentials[0] = credentials[0].toLowerCase();
+                    char[] pass = new char[credentials[1].length()];
+                    for (int i=0; i<pass.length; i++) {
+                        pass[i] = credentials[1].charAt(i);
+                    }
+                    if (allAccounts.stream().filter(a -> a.getEmail_address().equals(credentials[0])).count() <= 0) {
+
+                        //TODO: Create a new Account with credentials[0] and pass as the parameters
+                        //TODO: Add the new account to allAccounts
+                        //TODO: .put a new entry into loggedInAccounts using connection and the newly created Account as the parameters
+                        //TODO: Call the connection#replyCommand method with "login" as the command and "valid" as the argument
+
+                        try { saveAllAccounts(ACCOUNT_FILENAME);
+                        } catch (IOException e) { e.printStackTrace(); }
+                    } else {
+
+                        //TODO: Call the connection#replyCommand method with "login" as the command and "invalid" as the argument
+                    }
+                    break;
+
+                case "delete-email":
+
+                    //TODO: Retrieve the account from loggedInAccounts
+                    //TODO: Retrieve the email from the Account using the UUID stored in the command argument
+                    //TODO: Remove the email from the Account
+
+                    try { saveAllAccounts(ACCOUNT_FILENAME);
+                    } catch (IOException e) { e.printStackTrace(); }
+                    break;
+
+                case "read-email":
+
+                    //TODO: Retrieve the Account from loggedInAccounts
+                    //TODO: Retrieve the email from the Account using the UUID stored in the command argument
+                    //TODO: If the Email exists, call the Email#setHasOpened method and set it to true
+
+                    try { saveAllAccounts(ACCOUNT_FILENAME);
+                    } catch (IOException e) { e.printStackTrace(); }
+                    break;
+
+                case "get-emails":
+                    Account ac = loggedInAccounts.getOrDefault(connection, null);
+                    if (ac != null) connection.replyCommand("email-list", new Gson().toJson(ac.getAllReceivedEmails()));
+                    break;
+            }
         };
     }
 
@@ -175,14 +236,24 @@ public class EmailServer {
         return email -> {
 
             String[] recipients = email.getRecipients();
-            //TODO: put a deep copy " Email newEmail = new Email( oldEmail ); in each of the recipients' accounts"
-            // filter out recipients that are duplicates or ones that don't exist
-            // don't make it specific to capital or lowercase letters
-            // notify accounts that are currently logged in that they have a new email
+
+            //TODO: Make every recipient all lowercase
+            //TODO: Remove any duplicates from recipients
+            //TODO: For each unique recipient that exists within allAccounts, put a deep copy " Email newEmail = new Email( oldEmail ); " into the recipient's account
+            //TODO: If the Account is located in loggedInAccounts, use the getConnectionFromAccount method to obtain the logged-in connection...
+            //TODO: ...call the connection#replyEmail with email as the parameter
 
             // don't let this method throw an uncaught exception
 
+            try { saveAllAccounts(ACCOUNT_FILENAME);
+            } catch (IOException e) { e.printStackTrace(); }
         };
+    }
+
+    private Optional<Connection> getConnectionFromAccount(Account account) {
+        return loggedInAccounts.entrySet().stream()
+                .filter(e -> e.getValue().equals(account))
+                .findFirst().map(Map.Entry::getKey);
     }
 
 }
